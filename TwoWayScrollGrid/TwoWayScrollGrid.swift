92b100cfe30d442bc3df504aa57736f0b9fa078a
Pradeep <pradeepnswamy@gmail.com>
2020-01-14 23:26:55 +0530

    Added unit test

---
+++ TwoWayScrollGrid/TwoWayScrollGrid.swift
@@ -0,0 +1,22 @@
+//
+//  TwoWayScrollGrid.swift
+//  TwoWayScrollGrid
+//
+//  Created by Pradeep on 1/14/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+import Foundation
+
+public final class TwoWayScrollGrid {
+    
+    let name = "New Pods"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
