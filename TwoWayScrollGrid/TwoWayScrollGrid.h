d873d945c03b9285ac24eee5af5a44fe9e1c95a0
Pradeep <pradeepnswamy@gmail.com>
2020-01-14 23:09:33 +0530

    Initial project setup

---
+++ TwoWayScrollGrid/TwoWayScrollGrid.h
@@ -0,0 +1,19 @@
+//
+//  TwoWayScrollGrid.h
+//  TwoWayScrollGrid
+//
+//  Created by Pradeep on 1/14/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TwoWayScrollGrid.
+FOUNDATION_EXPORT double TwoWayScrollGridVersionNumber;
+
+//! Project version string for TwoWayScrollGrid.
+FOUNDATION_EXPORT const unsigned char TwoWayScrollGridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwoWayScrollGrid/PublicHeader.h>
+
+
